78734a022a303f9859e7aaf9e12276c27869e499
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-09-06 00:11:50 +0400

    some redirect to this http for test purposes

--- starter-code/js/data_fetcher.js
+++ starter-code/js/data_fetcher.js
@@ -5,7 +5,7 @@ export class DataFetcher {
 
     async fetchData() {
         try {
-            const response = await fetch("http://galleriawebsite-env.eba-wpfg4hzg.eu-north-1.elasticbeanstalk.com//get_images");
+            const response = await fetch("/api/get_images");
             if (!response.ok) {
                 throw new Error("Failed to fetch data. Please check you json file");
             }
